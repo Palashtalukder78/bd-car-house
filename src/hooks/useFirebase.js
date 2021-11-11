c79a3aa43c2f7b22df305e86edb90d272884928f
Palashtalukder78 <palashtalukder78@gmail.com>
2021-11-11 12:50:43 +0600

    update username in firebase and save user info in database

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
 import firebaseAuthentication from "../components/Login/Firebase/Firebase.init";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile, signOut } from "firebase/auth";
 import swal from 'sweetalert';
 import { useEffect, useState } from "react";
 firebaseAuthentication();
@@ -12,6 +12,18 @@ const useFirebase = () => {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 console.log(userCredential.user);
+                //update userName
+                const newUser = { email, displayName: displayName }
+                setUser(newUser)
+                //insert userInfo in database
+                saveUser(email, displayName, "POST")
+                updateProfile(auth.currentUser, {
+                    displayName: displayName
+                }).then(() => {
+                    console.log("update User");
+                }).catch((error) => {
+                    console.log(error.message);
+                });
                 const redirect_url = location?.state?.from || '/home'
                 history.replace(redirect_url)
                 swal("Welcome", "Account Create Successfully", "success");
@@ -21,6 +33,16 @@ const useFirebase = () => {
                 swal("!", "Something went", "error");
             })
             .finally(() => setIsLoading(false));
+    }
+    const saveUser = (email, displayName, method) => {
+        const user = { email, displayName }
+        fetch('http://localhost:5000/users', {
+            method: method,
+            headers: {
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(user)
+        })
     }
     const loginUser = (email, password, history, location) => {
         setIsLoading(true)
